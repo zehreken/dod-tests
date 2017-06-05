8e5ce0507f612309efd0c0c2ada4fc18cdfa9d67
zehreken <zehreken@gmail.com>
2017-06-06 01:28:45 +0300

    Update main, now it proves the point

--- main.cpp
+++ main.cpp
@@ -31,39 +31,74 @@ int Big::getSizeInKB()
 
 int Big::getSizeInMB()
 {
-	return sizeof(Big) / 1024 / 1024;
+	return sizeof(Big) / (1024 * 1024);
+}
+
+class Small
+{
+	private:
+		int actor;
+	public:
+		void setActor(int size);
+		int getActor();
+		int getSizeInKB();
+		int getSizeInMB();
+};
+
+void Small::setActor(int actor)
+{
+	this->actor = actor;
+}
+
+int Small::getActor()
+{
+	return this->actor;
+}
+
+int Small::getSizeInKB()
+{
+	return sizeof(Small) / 1024;
+}
+
+int Small::getSizeInMB()
+{
+	return sizeof(Small) / (1024 * 1024);
 }
 
 int main()
 {
-		using namespace std;
-		
-		const int bigCount = 1024;
-		Big* bigs = new Big[bigCount];
+	using namespace std;
+	
+	const int SIZE = 10240;
+	
+	Big* bigs = new Big[SIZE];
+	clock_t t1, t2;
+	t1 = clock();
+	for (int i = 0; i < SIZE; i++)
+	{
+		bigs[i].setActor(i);
+	}
+	t2 = clock();
+	float diff = ((float)t2 - (float)t1) / CLOCKS_PER_SEC;
+	cout << "Running time: " << diff << endl;
+	cout << "Address of bigs: " << &bigs << endl;
+	
+	// ==========
+	
+	Small* smalls = new Small[SIZE];
+	clock_t t3, t4;
+	t3 = clock();
+	for (int i = 0; i < SIZE; i++)
+	{
+		smalls[i].setActor(i);
+	}
+	t4 = clock();
+	diff = ((float)t4 - (float)t3) / CLOCKS_PER_SEC;
+	cout << "Running time: " << diff << endl;
+	cout << "Address of smalls: " << &smalls << endl;
 
-		clock_t t1,t2;
-		t1=clock();
-		
-		for (int i = 0; i < bigCount; i++)
-		{
-			bigs[i].setActor(i);
-		}
-		
-		/*
-		for (int i = 0; i < bigCount; i++)
-		{
-			cout << "Actor: " << bigs[i].getActor() << "\t Size of Big(KB): " << bigs[i].getSizeInKB() << endl;
-		}
-		*/
-				
-		t2=clock();
-		float diff = ((float)t2 - (float)t1) / CLOCKS_PER_SEC;
-		cout << "Running time: " << diff << endl;
-
-		
-		cout << "Address of bigs: " << &bigs << endl;
-		cout << "End of program" << endl;
-		
-		return 0;
+	cout << "End of program" << endl;
+	
+	return 0;
 }
 
